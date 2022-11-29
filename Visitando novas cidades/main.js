9c509a2712d3c5893e9f34abff06a2eab97d5c1e
Carolline Martins <106552762+carollinemv@users.noreply.github.com>
2022-11-29 17:11:42 -0300

    Add files via upload

---
+++ Visitando novas cidades/main.js
@@ -0,0 +1,9 @@
+const turista = prompt("E aí, turista! Qual é seu nome?")
+let cidades = ""
+let contagem = 0
+
+let continuar = prompt("Você visitou alguma cidade? (Sim/Não")
+
+while (continuar === "Sim") {
+    let cidade = prompt("Qual é o nome da cidade visitada?")
+}
