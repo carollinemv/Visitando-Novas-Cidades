3738ef623fe81c185ba4f7f466b0cf25d70c4d60
Carolline Martins <106552762+carollinemv@users.noreply.github.com>
2022-11-29 17:18:35 -0300

    Add files via upload

--- Visitando novas cidades/main.js
+++ Visitando novas cidades/main.js
@@ -6,4 +6,8 @@ let continuar = prompt("Você visitou alguma cidade? (Sim/Não")
 
 while (continuar === "Sim") {
     let cidade = prompt("Qual é o nome da cidade visitada?")
+    // cidades = cidades + ...
+    cidades += " - " + cidade + "\n"
+    contagem++
+    continuar = prompt("Você visitou alguma outra cidade?(Sim/Não)")
 }
